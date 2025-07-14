38959c1d9507491df71daab259a48ccc3a4fd76f
jay-1985 <gadhvijay130@gmail.com>
2025-07-14 18:29:23 +0530

    service section hide

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ const App = () => {
           <Route path="/about" element={<About />} />
           <Route path="/services" element={
           <>
-            <Services />
+            {/* <Services /> */}
             <Cars/>
           </>
           } 
